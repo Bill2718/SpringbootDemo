f20b7b94821b87dae768519134ebce230ec3b511
yi.li <yi.li2718@gmail.com>
2018-04-12 19:47:16 +0300

    first commit

---
+++ src/main/java/com/fy/spdemo/App.java
@@ -0,0 +1,13 @@
+package com.fy.spdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!1" );
+    }
+}
